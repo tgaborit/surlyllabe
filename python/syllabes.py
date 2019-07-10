5276c9781fe0d69cf133be92abf5d49b05dfe7a7
Titouan Gaborit <43585828+tgaborit@users.noreply.github.com>
2019-07-10 16:56:22 +0200

    add replacement function

--- python/syllabes.py
+++ python/syllabes.py
@@ -2,9 +2,13 @@
 
 """ Module permettant de décomposer un mot français en syllabes"""
 
+import re
+
 
 def get_vowel_consonant(word):
-    """ Fonction de conversion du mot en chaîne de voyelles et consonnes."""
+    """
+    Fonction de conversion du mot en chaîne de voyelles et consonnes.
+    """
     # Set des voyelles
     vowel = {'a', 'e', 'i', 'o', 'u', 'y'}
 
@@ -23,10 +27,30 @@ def get_vowel_consonant(word):
 
     return vowel_consonant_form
 
+
+def replace_on(on_this, base, to_find, by):
+    """
+    Recherche et remplacement de caractères dans une chaîne
+    en fonction d'un pattern trouvé dans une autre chaîne
+ 
+    -Usage-
+    replace_on(get_vowel_consonant("chat"), "chat", "ch", "gg")
+    """
+
+    # Recherche du pattern
+    pos = [(m.start(), m.end()) for m in re.finditer(to_find, base) ]
+
+    # Remplacement à la position correspondante dans l'autre chaîne
+    for start, end in pos:
+        on_this = on_this[0:start] + by + on_this[end:]
+     
+    return on_this
+
+
 # test du module syllabes
 if __name__ == "__main__":
     chaine = "test"
+    
     # test fonction get_vowel_consonant
     print("get_vowel_consonant({}) = {}".format(chaine,
                                                 get_vowel_consonant(chaine)))
-    
