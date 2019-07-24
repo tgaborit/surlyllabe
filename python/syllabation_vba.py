1912a2972a813c412da6e3ae876fbb79d40f086f
Titouan Gaborit <43585828+tgaborit@users.noreply.github.com>
2019-07-24 20:05:04 +0200

    add argument handling in python script

--- python/syllabation_vba.py
+++ python/syllabation_vba.py
@@ -2,3 +2,9 @@
 
 import argparse
 from syllabes import *
+
+parser = argparse.ArgumentParser()
+parser.add_argument("word", type=str)
+args = parser.parse_args()
+ret_value = syllabation(args.word)
+print(ret_value)
