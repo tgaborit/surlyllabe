87e76a67382874758c4fcaf8dd6b3638cb52563e
Titouan Gaborit <43585828+tgaborit@users.noreply.github.com>
2019-07-26 19:50:12 +0200

    add syllabic coding in encoder

--- python/syllabation_encoder.py
+++ python/syllabation_encoder.py
@@ -6,5 +6,10 @@ from syllabes import *
 parser = argparse.ArgumentParser()
 parser.add_argument("word", type=str)
 args = parser.parse_args()
-ret_value = syllabation(args.word)
+
+ret_value = ""
+
+for syllabe in syllabation(args.word):
+    ret_value += str(len(syllabe))
+    
 print(ret_value)
