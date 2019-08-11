f754a52a7af4650f29a36b90292d71443be2e98e
Titouan Gaborit <43585828+tgaborit@users.noreply.github.com>
2019-08-11 15:41:00 +0200

    add confirmation before copy in setup

--- setup.bat
+++ setup.bat
@@ -1,4 +1,4 @@
-XCOPY word\Surlyllabe.dotm %AppData%\Microsoft\Word\STARTUP
+XCOPY word\Surlyllabe.dotm %AppData%\Microsoft\Word\STARTUP /W
 XCOPY python\syllabes.py %AppData%\Microsoft\Word\Python\
 XCOPY python\syllabation_encoder.py %AppData%\Microsoft\Word\Python\
 PAUSE
