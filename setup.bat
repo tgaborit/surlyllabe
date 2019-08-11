867566d5603b0d7276932e5de0b77f0157c5b3c0
Titouan Gaborit <43585828+tgaborit@users.noreply.github.com>
2019-08-11 15:47:20 +0200

    add ending message

--- setup.bat
+++ setup.bat
@@ -1,4 +1,5 @@
 XCOPY word\Surlyllabe.dotm %AppData%\Microsoft\Word\STARTUP /W
 XCOPY python\syllabes.py %AppData%\Microsoft\Word\Python\
 XCOPY python\syllabation_encoder.py %AppData%\Microsoft\Word\Python\
+ECHO My work here is done. Now it is time for me to retire.
 PAUSE
