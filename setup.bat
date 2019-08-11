c9bb790789b7536499fb176a2eb075c575af539c
Titouan Gaborit <43585828+tgaborit@users.noreply.github.com>
2019-08-11 16:02:59 +0200

    add blank lines display

--- setup.bat
+++ setup.bat
@@ -1,5 +1,11 @@
+ECHO OFF
+ECHO.
 XCOPY word\Surlyllabe.dotm %AppData%\Microsoft\Word\STARTUP /W
+ECHO.
 XCOPY python\syllabes.py %AppData%\Microsoft\Word\Python\
+ECHO.
 XCOPY python\syllabation_encoder.py %AppData%\Microsoft\Word\Python\
+ECHO.
 ECHO My work here is done. Now it is time for me to retire.
+ECHO.
 PAUSE
