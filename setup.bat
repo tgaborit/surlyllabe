ef85e9526884e2227cd94fd61d63cafc9949376d
Titouan Gaborit <43585828+tgaborit@users.noreply.github.com>
2019-08-11 16:03:42 +0200

    add comments to batch file

--- setup.bat
+++ setup.bat
@@ -1,11 +1,16 @@
 ECHO OFF
 ECHO.
+
+:: Word file and Python files copy
 XCOPY word\Surlyllabe.dotm %AppData%\Microsoft\Word\STARTUP /W
 ECHO.
 XCOPY python\syllabes.py %AppData%\Microsoft\Word\Python\
 ECHO.
 XCOPY python\syllabation_encoder.py %AppData%\Microsoft\Word\Python\
 ECHO.
+
+:: End message
 ECHO My work here is done. Now it is time for me to retire.
 ECHO.
+
 PAUSE
