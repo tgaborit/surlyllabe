ef179b7de01143794d9e2614c78b0e82490717af
Titouan Gaborit <43585828+tgaborit@users.noreply.github.com>
2019-08-11 15:35:06 +0200

    add python files copy

--- setup.bat
+++ setup.bat
@@ -1,2 +1,4 @@
 XCOPY word\Surlyllabe.dotm %AppData%\Microsoft\Word\STARTUP
+XCOPY python\syllabes.py %AppData%\Microsoft\Word\Python\
+XCOPY python\syllabation_encoder.py %AppData%\Microsoft\Word\Python\
 PAUSE
